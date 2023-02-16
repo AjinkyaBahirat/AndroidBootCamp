822a9faa8c00251860e6e319387e61a1aa2c0ecc
AjinkyaBahirat <ajinkyabahiratppr@gmail.com>
2023-02-16 20:42:15 +0530

    Firebase Authentication

--- app/src/main/java/com/project/projectt/Register.java
+++ app/src/main/java/com/project/projectt/Register.java
@@ -3,13 +3,11 @@ package com.project.projectt;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,52 +16,42 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class Register extends AppCompatActivity {
+
     EditText email,pass;
-    Button login;
+    Button register;
     FirebaseAuth auth;
-    TextView gotoregister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
         email = findViewById(R.id.email);
         pass = findViewById(R.id.password);
-        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
         auth = FirebaseAuth.getInstance();
-        gotoregister = findViewById(R.id.gotoregister);
 
-        login.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //login button clicked..
                 if(TextUtils.isEmpty(email.getText()) || TextUtils.isEmpty(pass.getText())){
-                    Toast.makeText(MainActivity.this, "Please Enter All Fields..", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Register.this, "Please Enter All Fields..", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    auth.signInWithEmailAndPassword(email.getText().toString(),pass.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    auth.createUserWithEmailAndPassword(email.getText().toString(),pass.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
-                                Toast.makeText(MainActivity.this, "Login Success..!", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(Register.this, "User Created Successfully..", Toast.LENGTH_SHORT).show();
                             }
                         }
                     }).addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(MainActivity.this, "Error:"+e.getMessage(), Toast.LENGTH_LONG).show();
+                            Toast.makeText(Register.this, "Error:"+e.getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     });
                 }
-            }
-        });
-
-        gotoregister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,Register.class);
-                startActivity(intent);
             }
         });
     }
